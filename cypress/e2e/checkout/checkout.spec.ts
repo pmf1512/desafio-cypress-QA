18e515b3076aa1e6856ec99c3abb27b9ae6d11b6
germannaa <pamelamaia_freitas@hotmail.com>
2025-03-06 23:59:35 -0300

    ajuste checkout

--- cypress/e2e/checkout/checkout.spec.ts
+++ cypress/e2e/checkout/checkout.spec.ts
@@ -3,9 +3,9 @@ import InventoryPage from "../../support/pageObjects/InventoryPage";
 import LoginPage from "../../support/pageObjects/LoginPage";
 
 describe("Testes de Checkout", () => {
-    
+
     beforeEach(() => {
-        cy.visit("/");
+        LoginPage.visit();
         cy.url().should("include", Cypress.env("baseUrl"));
 
         LoginPage.login(Cypress.env("username"), Cypress.env("password"));
@@ -15,7 +15,7 @@ describe("Testes de Checkout", () => {
     });
 
     it("Deve finalizar a compra com sucesso", () => {
-        CheckoutPage.fillCheckoutForm(Cypress.env("firstName"),  Cypress.env("lastName"),  Cypress.env("zipCode"));
+        CheckoutPage.fillCheckoutForm(Cypress.env("firstName"), Cypress.env("lastName"), Cypress.env("zipCode"));
         CheckoutPage.confirmCheckout();
         CheckoutPage.getSuccessMessage().should("contain", "Thank you for your order!");
     });
@@ -28,21 +28,21 @@ describe("Testes de Checkout", () => {
     });
 
     it("Deve exibir erro ao tentar continuar sem preencher o primeiro nome", () => {
-        CheckoutPage.fillCheckoutForm("",  Cypress.env("lastName"),  Cypress.env("zipCode"));
+        CheckoutPage.fillCheckoutForm("", Cypress.env("lastName"), Cypress.env("zipCode"));
         CheckoutPage.getErrorMessage()
             .should("be.visible")
             .and("contain", "Error: First Name is required");
     });
 
     it("Deve exibir erro ao tentar continuar sem preencher o último nome", () => {
-        CheckoutPage.fillCheckoutForm(Cypress.env("firstName"), "",  Cypress.env("zipCode")); 
+        CheckoutPage.fillCheckoutForm(Cypress.env("firstName"), "", Cypress.env("zipCode"));
         CheckoutPage.getErrorMessage()
             .should("be.visible")
             .and("contain", "Error: Last Name is required");
     });
 
     it("Deve exibir erro ao tentar continuar sem preencher o CEP", () => {
-        CheckoutPage.fillCheckoutForm(Cypress.env("firstName"),  Cypress.env("lastName"), ""); 
+        CheckoutPage.fillCheckoutForm(Cypress.env("firstName"), Cypress.env("lastName"), "");
         CheckoutPage.getErrorMessage()
             .should("be.visible")
             .and("contain", "Error: Postal Code is required");
